7bf7bd85b0d611eaab8e67b856c0781155e6db4a
Kai <kai@moltbot>
2026-05-25 11:20:39 +1000

    openclaw: revert ssh_key_path to pve-kai (id_ed25519 not accessible on runner)

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -284,7 +284,7 @@ module "openclaw" {
 
   # Mode A: cloud-image provisioner credentials
   proxmox_host  = "192.168.1.50"
-  ssh_key_path  = "/home/moltbot/.ssh/id_ed25519"
+  ssh_key_path  = "/home/moltbot/.ssh/pve-kai"
   proxmox_node  = "pve"
 
   # Cloud-init admin user and SSH key
